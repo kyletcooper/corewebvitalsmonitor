ca3ece29b73eb480435608fa7e43125099a7cfda
kyle <kyle@webresultsdirect.com>
2023-01-11 06:05:04 -0500

    .

--- src/class-assets-manager.php
+++ src/class-assets-manager.php
@@ -2,56 +2,64 @@
 
 namespace corewebvitalsmonitor;
 
-class Assets_Manager {
+class Assets_Manager
+{
 
-	function __construct() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_public' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin' ) );
-		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor' ) );
+	function __construct()
+	{
+		add_action('wp_enqueue_scripts', array($this, 'enqueue_public'));
+		add_action('admin_enqueue_scripts', array($this, 'enqueue_admin'));
+		add_action('enqueue_block_editor_assets', array($this, 'enqueue_block_editor'));
 	}
 
-	function enqueue_public() {
-		 $ver = Plugin_Manager::VERSION;
+	function enqueue_public()
+	{
+		$ver = Plugin_Manager::VERSION;
 
-		wp_enqueue_script( 'cwvm-monitor', plugin_dir_url( Plugin_Manager::FILE ) . 'assets/scripts/dist/public.js', array(), $ver, true );
+		wp_enqueue_script('cwvm-monitor', plugin_dir_url(Plugin_Manager::FILE) . 'assets/scripts/dist/public.js', array(), $ver, true);
 
 		wp_localize_script(
 			'cwvm-monitor',
 			'CWVM',
 			array(
 				'rest_url'   => rest_url(),
-				'rest_nonce' => wp_create_nonce( 'wp_rest' ),
+				'rest_nonce' => wp_create_nonce('wp_rest'),
 				'admin_url'  => admin_url(),
 			)
 		);
 
-		$this->set_script_loading_mode( 'cwvm-monitor', 'async' );
+		$this->set_script_loading_mode('cwvm-monitor', 'async');
 	}
 
-	function enqueue_admin() {
+	function enqueue_admin()
+	{
 		$ver = Plugin_Manager::VERSION;
 
-		wp_enqueue_script( 'cwvm-metabox', plugin_dir_url( Plugin_Manager::FILE ) . 'assets/scripts/dist/admin.js', array(), $ver, false );
+		wp_enqueue_script('cwvm-metabox', plugin_dir_url(Plugin_Manager::FILE) . 'assets/scripts/dist/admin.js', array(), $ver, false);
 
 		wp_localize_script(
 			'cwvm-metabox',
 			'CWVM',
 			array(
 				'rest_url'   => rest_url(),
-				'rest_nonce' => wp_create_nonce( 'wp_rest' ),
+				'rest_nonce' => wp_create_nonce('wp_rest'),
+				'admin_url'  => admin_url(),
 			)
 		);
 	}
 
-	function enqueue_block_editor() {   }
+	function enqueue_block_editor()
+	{
+	}
 
-	function set_script_loading_mode( $handle, $loading_mode = 'sync' ) {
+	function set_script_loading_mode($handle, $loading_mode = 'sync')
+	{
 		add_filter(
 			'script_loader_tag',
-			function ( $tag, $found_handle, $src ) use ( $handle, $loading_mode ) {
-				if ( $found_handle === $handle ) {
-					$loading_mode = esc_attr( $loading_mode );
-					return str_replace( '></script>', " $loading_mode></script>", $tag );
+			function ($tag, $found_handle, $src) use ($handle, $loading_mode) {
+				if ($found_handle === $handle) {
+					$loading_mode = esc_attr($loading_mode);
+					return str_replace('></script>', " $loading_mode></script>", $tag);
 				}
 
 				return $tag;
