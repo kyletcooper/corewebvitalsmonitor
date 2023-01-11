01b359cfb397e1b840d9710231328ed5ce059729
Kyle Cooper <kyle@webresultsdirect.com>
2023-01-11 12:31:29 +0000

    Formatting

--- corewebvitalsmonitor.php
+++ corewebvitalsmonitor.php
@@ -16,13 +16,11 @@ namespace corewebvitalsmonitor;
  * Domain Path:       /languages
  */
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-class Plugin_Manager
-{
-
+class Plugin_Manager {
 	const DIR     = __DIR__;
 	const FILE    = __FILE__;
 	const VERSION = '1.0.0';
@@ -32,69 +30,60 @@ class Plugin_Manager
 	public Metabox $metabox;
 	public Routes_Manager $routes_manager;
 
-	function __construct()
-	{
-		require_once plugin_dir_path(static::FILE) . '/src/class-metrics-table.php';
+	function __construct() {
+		require_once plugin_dir_path( static::FILE ) . '/src/class-metrics-table.php';
 
-		register_activation_hook(__FILE__, array(static::class, 'activate'));
-		register_uninstall_hook(__FILE__, array(static::class, 'uninstall'));
+		register_activation_hook( __FILE__, array( static::class, 'activate' ) );
+		register_uninstall_hook( __FILE__, array( static::class, 'uninstall' ) );
 
-		if (!is_admin()) {
-			add_action('init', array($this, 'init_public'));
+		if ( ! is_admin() ) {
+			add_action( 'init', array( $this, 'init_public' ) );
 		}
 
-		add_action('init', array($this, 'init'));
-		add_action('admin_init', array($this, 'init_admin'));
-		add_action('rest_api_init', array($this, 'init_rest'));
+		add_action( 'init', array( $this, 'init' ) );
+		add_action( 'admin_init', array( $this, 'init_admin' ) );
+		add_action( 'rest_api_init', array( $this, 'init_rest' ) );
 	}
 
-	function init()
-	{
-		require_once plugin_dir_path(static::FILE) . '/src/class-assets-manager.php';
+	function init() {
+		require_once plugin_dir_path( static::FILE ) . '/src/class-assets-manager.php';
 		$this->asset_manager = new Assets_Manager();
 
-		require_once plugin_dir_path(static::FILE) . '/src/class-admin-dashboard.php';
+		require_once plugin_dir_path( static::FILE ) . '/src/class-admin-dashboard.php';
 		$this->admin_dashboard = new Admin_Dashboard();
 	}
 
-	function init_admin()
-	{
-		require_once plugin_dir_path(static::FILE) . '/src/class-metabox.php';
+	function init_admin() {
+		require_once plugin_dir_path( static::FILE ) . '/src/class-metabox.php';
 		$this->metabox = new Metabox();
 	}
 
-	function init_public()
-	{
-	}
+	function init_public() {    }
 
-	function init_rest()
-	{
-		require_once plugin_dir_path(static::FILE) . '/src/class-routes-manager.php';
+	function init_rest() {
+		require_once plugin_dir_path( static::FILE ) . '/src/class-routes-manager.php';
 		$this->routes_manager = new Routes_Manager();
 	}
 
-	static function activate()
-	{
+	static function activate() {
 		$metrics_table = Metrics_Table::get_instance();
 		$metrics_table->create_table();
 
-		update_option('corewebvitalsmonitor_version', static::VERSION);
+		update_option( 'corewebvitalsmonitor_version', static::VERSION );
 	}
 
-	static function uninstall()
-	{
+	static function uninstall() {
 		$metrics_table = Metrics_Table::get_instance();
 		$metrics_table->drop_table();
 
-		delete_option('corewebvitalsmonitor_version');
+		delete_option( 'corewebvitalsmonitor_version' );
 	}
 }
 
-function corewebvitalsmonitor()
-{
+function corewebvitalsmonitor() {
 	global $corewebvitalsmonitor;
 
-	if (!isset($corewebvitalsmonitor)) {
+	if ( ! isset( $corewebvitalsmonitor ) ) {
 		$corewebvitalsmonitor = new Plugin_Manager();
 	}
 
